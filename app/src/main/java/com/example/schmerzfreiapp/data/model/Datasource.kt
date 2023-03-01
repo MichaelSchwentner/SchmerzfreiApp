5a2cebd501ff7800ec8f06c2479d1ce7e9f9def6
michaelschwentner <mid.schwentner@gmail.com>
2023-03-01 15:05:12 +0100

    update.v6 recycler

--- app/src/main/java/com/example/schmerzfreiapp/data/model/Datasource.kt
+++ app/src/main/java/com/example/schmerzfreiapp/data/model/Datasource.kt
@@ -9,15 +9,11 @@ class Datasource {
             Uebungen(R.drawable.group_35555, "WARM UP / COOL DOWN"),
             Uebungen(R.drawable.group_35555, "BASISÜBUNGEN"),
             Uebungen(R.drawable.group_35555, "BECKEN UNTERER RÜCKEN"),
-            Uebungen(R.drawable.group_35555, "WARM UP / COOL DOWN"),
-            Uebungen(R.drawable.group_35555, "BASISÜBUNGEN"),
-            Uebungen(R.drawable.group_35555, "BECKEN UNTERER RÜCKEN"),
-            Uebungen(R.drawable.group_35555, "WARM UP / COOL DOWN"),
-            Uebungen(R.drawable.group_35555, "BASISÜBUNGEN"),
-            Uebungen(R.drawable.group_35555, "BECKEN UNTERER RÜCKEN"),
-            Uebungen(R.drawable.group_35555, "WARM UP / COOL DOWN"),
-            Uebungen(R.drawable.group_35555, "BASISÜBUNGEN"),
-            Uebungen(R.drawable.group_35555, "BECKEN UNTERER RÜCKEN")
+            Uebungen(R.drawable.group_35555, "SCHULTERN"),
+            Uebungen(R.drawable.group_35555, "ARME, HÄNDE"),
+            Uebungen(R.drawable.group_35555, "NACKEN, HWS"),
+            Uebungen(R.drawable.group_35555, "AUGEN, KIEFER"),
+            Uebungen(R.drawable.group_35555, "FÜSSE, KNIE")
         )
     }
 }
