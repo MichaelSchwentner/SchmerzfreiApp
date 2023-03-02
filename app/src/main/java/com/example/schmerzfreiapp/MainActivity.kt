558e9036d99f74797912ffe5f75466709f652412
michaelschwentner <mid.schwentner@gmail.com>
2023-03-02 08:52:03 +0100

    update.v10 recycler

--- app/src/main/java/com/example/schmerzfreiapp/MainActivity.kt
+++ app/src/main/java/com/example/schmerzfreiapp/MainActivity.kt
@@ -37,8 +37,8 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        //// Passing each menu ID as a set of Ids because each
+        //// menu should be considered as top level destinations.
 
         navView.setItemIconTintList(null);
         appBarConfiguration = AppBarConfiguration(
