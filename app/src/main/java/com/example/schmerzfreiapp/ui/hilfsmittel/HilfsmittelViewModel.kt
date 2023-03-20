6dfc5851d0be6b683dbf49e99adef0275b4a7d51
michaelschwentner <mid.schwentner@gmail.com>
2023-03-20 18:23:48 +0100

    update.v14 recycler

--- app/src/main/java/com/example/schmerzfreiapp/ui/hilfsmittel/HilfsmittelViewModel.kt
+++ app/src/main/java/com/example/schmerzfreiapp/ui/hilfsmittel/HilfsmittelViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HilfsmittelViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Hifsmittel Fragment"
+        value = "HILFSMITTEL ZUR SELBSTBEHANDLUNG"
     }
     val text: LiveData<String> = _text
 }
