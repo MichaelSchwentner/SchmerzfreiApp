5e205350f4da34bb7331804079cd87c2c98e7bdd
michaelschwentner <mid.schwentner@gmail.com>
2023-03-28 14:39:37 +0200

    update.v20 recycler

--- app/src/main/java/com/example/schmerzfreiapp/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/schmerzfreiapp/adapter/HomeAdapter.kt
@@ -5,38 +5,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schmerzfreiapp.R
 import com.example.schmerzfreiapp.data.model.Uebungen
-import com.example.schmerzfreiapp.ui.uebungen.UebungenFragment
 
-class UebungAdapter(
-    private val context: UebungenFragment,
+class HomeAdapter(
     private val dataset: List<Uebungen>
-) : RecyclerView.Adapter<UebungAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
     // IDEE EINES VIEWHOLDERS
     // der ViewHolder weiß welche Teile des Layouts beim Recycling angepasst werden
     class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val uebungsBild = view.findViewById<ImageView>(R.id.uebungsBild_image)
-        val uebungsTitel = view.findViewById<TextView>(R.id.uebungsTitel_text)
+        val bild = view.findViewById<ImageView>(R.id.imageView6)
+        val text = view.findViewById<TextView>(R.id.textView11)
+        val card = view.findViewById<CardView>(R.id.card2)
     }
     // ERSTELLEN DES VIEWHOLDERS
     // hier werden neue ViewHolder erstellt
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.uebungsbilder_item, parent, false)
+            .inflate(R.layout.rec2_item, parent, false)
         return ViewHolder(adapterLayout)
     }
-
-
     // BEFÜLLEN DES VIEWHOLDERS
     // hier findet der Recyclingprozess statt
     // die vom ViewHolder bereitgestellten Parameter werden verändert
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val uebungen = dataset[position]
 
-        holder.uebungsBild.setImageResource(uebungen.imageResource)
-        holder.uebungsTitel.setText(uebungen.namevideofile)
+        holder.bild.setImageResource(uebungen.imageResource)
+        holder.text.setText(uebungen.namevideofile)
 
     }
     // damit der LayoutManager weiß wie lang die Liste ist
--- app/src/main/java/com/example/schmerzfreiapp/adapter/HometwoAdapter.kt
+++ app/src/main/java/com/example/schmerzfreiapp/adapter/HometwoAdapter.kt
@@ -5,38 +5,37 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schmerzfreiapp.R
 import com.example.schmerzfreiapp.data.model.Uebungen
-import com.example.schmerzfreiapp.ui.uebungen.UebungenFragment
 
-class UebungAdapter(
-    private val context: UebungenFragment,
+class HometwoAdapter(
     private val dataset: List<Uebungen>
-) : RecyclerView.Adapter<UebungAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<HometwoAdapter.ViewHolder>() {
     // IDEE EINES VIEWHOLDERS
     // der ViewHolder weiß welche Teile des Layouts beim Recycling angepasst werden
     class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val uebungsBild = view.findViewById<ImageView>(R.id.uebungsBild_image)
-        val uebungsTitel = view.findViewById<TextView>(R.id.uebungsTitel_text)
+        val bild = view.findViewById<ImageView>(R.id.imageView5)
+        val backgroundiv = view.findViewById<ImageView>(R.id.backgroundImage_iv)
+        val card2 = view.findViewById<CardView>(R.id.mv_card)
     }
     // ERSTELLEN DES VIEWHOLDERS
     // hier werden neue ViewHolder erstellt
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.uebungsbilder_item, parent, false)
+            .inflate(R.layout.image_item, parent, false)
         return ViewHolder(adapterLayout)
     }
-
-
     // BEFÜLLEN DES VIEWHOLDERS
     // hier findet der Recyclingprozess statt
     // die vom ViewHolder bereitgestellten Parameter werden verändert
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val uebungen = dataset[position]
 
-        holder.uebungsBild.setImageResource(uebungen.imageResource)
-        holder.uebungsTitel.setText(uebungen.namevideofile)
+        holder.bild.setImageResource(uebungen.imageResource)
+        holder.backgroundiv.setImageResource(uebungen.imageResource)
+        //holder.text.setText(uebungen.namevideofile)
 
     }
     // damit der LayoutManager weiß wie lang die Liste ist
