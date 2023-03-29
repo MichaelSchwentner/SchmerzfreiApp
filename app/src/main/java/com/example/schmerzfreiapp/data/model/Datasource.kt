0e80b36b707fb4d9e3362867bc56c17093771e2d
michaelschwentner <mid.schwentner@gmail.com>
2023-03-29 10:51:21 +0200

    update.v26 recycler

--- app/src/main/java/com/example/schmerzfreiapp/data/model/Datasource.kt
+++ app/src/main/java/com/example/schmerzfreiapp/data/model/Datasource.kt
@@ -18,14 +18,10 @@ class Datasource {
     }
     fun getUebungsbilder2(): List<Uebungen> {
         return listOf(
-            Uebungen(R.drawable.group_35558, "WARM UP / COOL DOWN"),
+            Uebungen(R.drawable.group_35554, "WARM UP / COOL DOWN"),
             Uebungen(R.drawable.group_35558, "BASISÜBUNGEN"),
-            Uebungen(R.drawable.group_35558, "BECKEN UNTERER RÜCKEN"),
-            Uebungen(R.drawable.group_35558, "SCHULTERN"),
-            Uebungen(R.drawable.group_35558, "ARME, HÄNDE"),
-            Uebungen(R.drawable.group_35558, "NACKEN, HWS"),
-            Uebungen(R.drawable.group_35558, "AUGEN, KIEFER"),
-            Uebungen(R.drawable.group_35558, "FÜSSE, KNIE")
+            Uebungen(R.drawable.group_35554, "BECKEN UNTERER RÜCKEN")
+
         )
     }
 }
