3a46bf31de788e62846104a2f56c79a702724d29
michaelschwentner <mid.schwentner@gmail.com>
2023-03-31 18:01:37 +0200

    update.v35 recycler

--- app/src/main/java/com/example/schmerzfreiapp/ui/gesundheitstipps/GesundheitstippsViewModel.kt
+++ app/src/main/java/com/example/schmerzfreiapp/ui/gesundheitstipps/GesundheitstippsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GesundheitstippsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Gesundheitstipps Fragment"
+        value = "GESUNDHEITSTIPPS"
     }
     val text: LiveData<String> = _text
 }
