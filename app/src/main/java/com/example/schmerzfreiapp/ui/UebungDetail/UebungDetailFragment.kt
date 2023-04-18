41aad85cbb3b00e2bc2d51f3675fac419818ec6a
michaelschwentner <mid.schwentner@gmail.com>
2023-04-18 12:58:51 +0200

    update.v52 recycler

--- app/src/main/java/com/example/schmerzfreiapp/ui/UebungDetail/UebungDetailFragment.kt
+++ app/src/main/java/com/example/schmerzfreiapp/ui/UebungDetail/UebungDetailFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.widget.TextView
+import android.widget.inline.InlineContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.schmerzfreiapp.R
 import com.example.schmerzfreiapp.databinding.FragmentUebungDetailBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -38,22 +41,35 @@ class UebungDetailFragment : Fragment() {
             textView.text = it
         }
         val uri = arguments?.getString("uri")
-        val playerView = binding.videoPlayer
-        val videoUri = MediaItem.fromUri(uri!!)
-
-        val dataSourceFactory = DefaultHttpDataSource.Factory()
-        val mediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri)
-
-        val player = ExoPlayer.Builder(requireContext()).build()
-        player.setMediaSource(mediaSource)
-
-// Attach player to your view or surface
-        playerView.player = player
-
-// Start playback when ready
-        player.playWhenReady = true
+        println(uri)
+//        val playerView = binding.videoPlayer
+//        val videoUri = MediaItem.fromUri(uri!!)
+//
+//        val dataSourceFactory = DefaultHttpDataSource.Factory()
+//        val mediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri)
+//
+//        val player = ExoPlayer.Builder(requireContext()).build()
+//        player.setMediaSource(mediaSource)
+//
+//// Attach player to your view or surface
+//        playerView.player = player
+//
+//// Start playback when ready
+//        player.playWhenReady = true
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val uri = arguments?.getString("uri")
+        var webview = binding.webViews
+        webview.settings.javaScriptEnabled=true
+        webview.webChromeClient = WebChromeClient()
+        webview.settings.domStorageEnabled = true
+
+        if (uri != null) {
+            webview.loadUrl(uri)
+        }
     }
 
     override fun onDestroyView() {
